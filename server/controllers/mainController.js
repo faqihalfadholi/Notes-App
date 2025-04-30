127e143716470bf5ec5ce7789da4e28cabb02531
Muhammad Faqih Al Fadholi <faqihalfadholi@gmail.com>
2025-04-30 21:28:41 +0700

    Initial commit

---
+++ server/controllers/mainController.js
@@ -0,0 +1,51 @@
+/**
+ * GET /
+ * Homepage 
+*/
+exports.homepage = async (req, res) => {
+  const locals = {
+    title: "NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('index', {
+    locals,
+    layout: '../views/layouts/front-page'
+  });
+}
+
+
+/**
+ * GET /
+ * About 
+*/
+exports.about = async (req, res) => {
+  const locals = {
+    title: "About - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('about', locals);
+}
+
+/**
+ * GET /
+ * Faqs 
+*/
+exports.faqs = async (req, res) => {
+  const locals = {
+    title: "FAQs - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('faqs', locals);
+}
+
+/**
+ * GET /
+ * Features
+*/
+exports.features = async (req, res) => {
+  const locals = {
+    title: "Features - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  }
+  res.render('features', locals);
+}
---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const mainController = require('../controllers/mainController');
+
+/**
+ * App Routes 
+*/
+router.get('/', mainController.homepage);
+router.get('/about', mainController.about);
+router.get('/faqs', mainController.faqs);
+router.get('/features', mainController.features);
+
+
+
+module.exports = router;
